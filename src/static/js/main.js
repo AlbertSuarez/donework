a26ab1440a864cce5c375c3783716e9f6d0914ae
adriacabeza <adriacabeza@hotmail.com>
2019-04-06 22:58:26 +0200

    te subo el index.html

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,1 +1,8 @@
-function generateText
+function run() {
+  document.getElementById("targetDiv").style.visibility = "visible"; 
+  var text = document.getElementById('textInput').value,
+      target = document.getElementById('targetDiv'),
+      converter = new showdown.Converter(),
+      html = converter.makeHtml(text);
+    target.innerHTML = html;
+}
