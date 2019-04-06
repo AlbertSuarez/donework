df921c8366a7e7a8b60403b96f8e7c21afa23ec7
laca <xlcbdn@gmail.com>
2019-04-07 00:50:02 +0200

    Button generate working (missing word2vec)

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -8,3 +8,31 @@ function run() {
       html = converter.makeHtml(text);
     target.innerHTML = html;
 }
+
+function generateText() {
+  textInput = document.getElementById('textInput');
+  text = textInput.value;
+  // Set up our HTTP request
+  var xhr = new XMLHttpRequest();
+
+  // Setup our listener to process completed requests
+  xhr.onload = function () {
+    // Process our return data
+    if (xhr.status >= 200 && xhr.status < 300) {
+      // What do when the request is successful
+      console.log('Text generated!');
+      textInput.value = text + xhr.responseText;
+    } else {
+      // What do when the request fails
+      console.log('The request failed!');
+    }
+    // Code that should run regardless of the request status
+  };
+
+  // Create and send a GET request
+  // The first argument is the post type (GET, POST, PUT, DELETE, etc.)
+  // The second argument is the endpoint URL
+  xhr.open('GET', 'http://0.0.0.0:6969/generate?inputText=' + text);
+  xhr.send();
+
+}
